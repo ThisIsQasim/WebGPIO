a4749b518ece183625dbf4d754b236ce22c1cf0f
ThisIsQasim <QasRoX@hotmail.com>
2017-05-29 23:50:26 +0500

    fixed errors in ipNotifier.sh

--- scripts/ipNotifier.sh
+++ scripts/ipNotifier.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 CLOUDHOST=aws.thisisqasim.com
-LANADDRESS=$(ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
+LANADDRESS=$(ip addr  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 WANADDRESS=$(curl http://icanhazip.com)
 
 curl http://{$CLOUDHOST}/ip/{$LANADDRESS}/
 
 while true; do 
 
-ip monitor address | while read  -t 120 output
+ip monitor address | while read -t 120 output
 do
  if echo $output | grep -v Deleted | grep eth0 | awk '{ print $4}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"; then
  LANADDRESS=$(echo $output | grep -v Deleted | grep eth0 | awk '{ print $4}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
