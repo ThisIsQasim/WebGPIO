af2f945d949128b3e3e95c4ff104e6fe841e8433
ThisIsQasim <QasRoX@hotmail.com>
2017-06-28 20:15:05 +0500

    added minimal ui to embed into other projects

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, Markup
+from datetime import timedelta
+from functools import update_wrapper
+from flask import Flask, render_template, redirect, Markup, make_response, request, current_app
 import RPi.GPIO as GPIO
 import subprocess, os, datetime, time
 app = Flask(__name__)
@@ -24,6 +26,47 @@ def accState(roomNumber, accNumber):
 		#get the state of other accesories in other rooms
 		return 'containerOff'
 
+
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, list):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, list):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
+	
+	def get_methods():
+		if methods is not None:
+			return methods
+		
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
+	
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
+			
+			h = resp.headers
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			h['Access-Control-Allow-Credentials'] = 'true'
+			h['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
+						
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
+
 @app.route("/")
 def main():
 	now = datetime.datetime.now()
@@ -44,7 +87,20 @@ def main():
 	}
 	return render_template('main.html', **templateData)
 
+@app.route("/grid/")
+@crossdomain(origin='*')
+def grid():
+	passer = ''
+	for i in range(len(roomName)):
+		passer = passer + "<p class='roomtitle'>%s</p>" % (roomName[i])
+		for j in range(len(accName[i])):
+			buttonHtmlName = accName[i][j].replace(" ", "<br>")
+			passer = passer + "<span id='button%d%d'><button class='%s' onclick='toggle(%d,%d)'>%s</button></span>" % (i, j, accState(i,j), i, j, buttonHtmlName)
+
+	return passer
+
 @app.route("/button/<int:roomNumber>/<int:accNumber>/")
+@crossdomain(origin='*')
 def toggle(roomNumber, accNumber):
 	if len(outPin[roomNumber]) != 0:
 		state= not GPIO.input(outPin[roomNumber][accNumber])
